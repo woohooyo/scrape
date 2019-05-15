6994ed495b92f5811caf8e8be30280b35fb44ccf
aliiwu <aliiwu@xogrp.com>
2019-05-15 12:27:06 +0800

    add timer and queue

--- src/scrape/index.ts
+++ src/scrape/index.ts
@@ -1,10 +1,24 @@
+import { mongoConfig } from '../config';
+import { Logger } from '../lib/logger';
+import { Queue } from '../mongo/models/queue';
 import { Scrape } from './scrape';
 
-export const runScrape = async() => {
+export const runScrape = async(operator = 'Scrape program') => {
+  let logger: Logger;
   try {
+    logger = new Logger(false);
+    const queueModel = new Queue(mongoConfig);
+    const insertKey = 'scrape_task';
+    const key = `${insertKey}_${new Date().getTime()}`;
+    await queueModel.insertOne({
+      key,
+      createdBy: operator,
+    });
     const scrape = new Scrape();
     await scrape.SyncProducts();
+    await queueModel.deleteOne({key});
   } catch (error) {
     console.log(error);
+    if (logger) { logger.error('run scrape error', error); }
   }
 };
